3d080bee3d3ca4b9b24f421cbb228a8106ce6a8b
Bohdan Kornienko <bohdankornienko@gmail.com>
2025-10-30 12:15:31 +0200

    Mockup objects and tweaking bot to look at the cursor

--- machines/bot.gd
+++ machines/bot.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 func process_input(delta: float):
     var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
     velocity = input_direction * move_speed * delta
+    look_at(get_global_mouse_position())
 
 func _physics_process(delta: float) -> void:
     process_input(delta)
